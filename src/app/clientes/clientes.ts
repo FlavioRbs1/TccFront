29305ad9b3e8e3878816f98d0cbc20b9a75cbc7e
Flavio Ribeiro <flavio.ribeiro@daxia.local>
2021-06-06 21:30:29 -0300

    Finalização tela de clientes

---
+++ src/app/clientes/clientes.ts
@@ -0,0 +1,13 @@
+export class Cliente{
+    id: number | undefined;
+    nome: string = "";
+    cpf: string  = "";
+    rg: string  = "";
+    sexo: string = "";
+    tel1: string = "";
+    tel2: string = "";
+    estadoCivil: string = "";
+    dtNasc: string = "";
+    comentarios:string = "";
+    
+}
