eaf8c211172700ce125f9176f19360f265131c88
FlavioRbs1 <52616379+FlavioRbs1@users.noreply.github.com>
2022-02-07 14:37:42 -0300

    Finalizado

---
+++ clientes-lista.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Router} from '@angular/router';
+import { ClientesService } from 'src/app/clientes/clientes.service';
+import { PedidoService } from 'src/app/pedido/pedido.service';
+import { Pedidos } from 'src/app/clientes/pedidos';
+import { Cliente } from '../clientes';
+
+@Component({
+  selector: 'app-clientes-lista',
+  templateUrl: './clientes-lista.component.html',
+  styleUrls: ['./clientes-lista.component.css']
+})
+export class ClientesListaComponent implements OnInit {
+
+  cliente: Cliente |any;
+  pedido: Pedidos |any;
+  idCliente:number|any;
+  id: number|any;
+  listaPedido:Pedidos[]|any;
+
+  
+  constructor(private service:ClientesService, private router: Router,private pedidoService:PedidoService) {
+    this.cliente = new Cliente();
+    this.pedido = new Pedidos();
+   }
+
+  ngOnInit(): void {
+    this.pedidoService.getPedidosEmCredito().subscribe(
+      response => this.listaPedido = response
+    );
+
+  }
+
+  onSubmit(){}
+
+  buscaCliente(){
+    const valor = Number(this.pedido.id);
+    this.pedidoService.buscaclientebypedido(valor).subscribe(
+      response =>{
+        this.idCliente = response;
+        window.location.href=`/clientes/form/${this.idCliente.cpf}`
+      },errorResponse => this.idCliente = null
+
+    );
+    ;
+  }
+
+}
