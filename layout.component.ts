65ef1aaf119f6a2366d02b42f4ca88373b40afd4
FlavioRbs1 <52616379+FlavioRbs1@users.noreply.github.com>
2022-02-07 14:42:42 -0300

    Finalizado

---
+++ layout.component.ts
@@ -0,0 +1,27 @@
+import { Component,AfterViewInit } from '@angular/core';
+import * as jquery from 'jquery';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent implements AfterViewInit {
+ 
+ 
+
+  constructor(
+
+  ) { 
+
+
+  }
+  ngAfterViewInit(): void {
+    throw new Error('Method not implemented.');
+  }
+ 
+
+}
+  
+
+
